3934db640785acd1a1967f7de0dc2b02418ba3ed
A-Abd <abrar.abdulwahed@gmail.com>
2022-12-29 12:20:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 20;
+let num2 = 3.1;
+let multiply = num1 * num2;
 
-// Write your code here
+let random =  Math.floor(Math.random() * 10);
+
+let num3 = 24;
+let num4 = 20;
+let mod = num3 % num4;
+
+let max =  Math.max(2, 5, 7, 10, 20);
